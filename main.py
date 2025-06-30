bc5a00e556512e90679bd8b2320a77df9b005e01
wasabii <wasabiizombie@gmail.com>
2025-06-30 04:13:19 -0500

    "0.0.2 - FPS set and close works.

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
-from constants import *
 
 def main():
-    pygame.init()  # Initialize all imported pygame modules
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0
 
     while True:
-          for event in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-          screen.fill((0, 0, 0))  # Fill the screen with black
-          pygame.display.flip()  # Update the display
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        tick = clock.tick(60)
+        
+        dt = tick / 1000.0
+
+
+
+    
 
 if __name__ == "__main__":
-        main()
+    main()
