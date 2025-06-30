fba2303f16447340620edf2056d944bf91d257ff
wasabii <wasabiizombie@gmail.com>
2025-06-30 04:43:14 -0500

    0.0.4 - added player!

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 import pygame
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         tick = clock.tick(60)
         
